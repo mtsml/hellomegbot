26f058e933ebb56d626ea4a35ce90aeaf8ce5892
matsu <50922910+mtsml@users.noreply.github.com>
2024-03-02 15:04:59 +0900

    ec2のファイル内容にあわせる

--- hellomeg.py
+++ hellomeg.py
@@ -1,9 +1,11 @@
 import os
 import random
+from dotenv import load_dotenv
 import discord
 from discord import app_commands
 
 
+load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 HELLO_MEG_SMALL = "ハロめぐー！"
 HELLO_MEG_MEDIUM = '''
@@ -12,7 +14,7 @@ HELLO_MEG_MEDIUM = '''
 ￣Y^Y^Y^Y^Y^Y^Y￣
 '''
 HEELO_MEG_LARGE = '''
-           
+​           
    ■       
    ■   ■   
    ■   ■   
@@ -77,11 +79,9 @@ async def on_ready():
 @tree.command(name="hellomeg",description="ハロめぐー！")
 async def hellomeg(interaction: discord.Interaction):
     rand_num = random.random()
-    message = HELLO_MEG_SMALL
-    if rand_num < 0.03:
+    message = HELLO_MEG_MEDIUM
+    if rand_num < 0.1:
         message = HEELO_MEG_LARGE
-    elif rand_num > 0.9:
-        message = HELLO_MEG_MEDIUM
     await interaction.response.send_message(message)
 
 

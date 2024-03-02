4968bf9210a4ed51d944b06a81cced9c26bb6f2e
matsu <50922910+mtsml@users.noreply.github.com>
2024-03-02 20:55:55 +0900

    add fever method

--- hellomeg.py
+++ hellomeg.py
@@ -1,8 +1,8 @@
-import os
-import random
+import os, io, random, unicodedata
 from dotenv import load_dotenv
 import discord
 from discord import app_commands
+from fever import generate_fever_png
 
 
 load_dotenv()
@@ -83,6 +83,23 @@ async def hellomeg(interaction: discord.Interaction):
     if rand_num < 0.1:
         message = HEELO_MEG_LARGE
     await interaction.response.send_message(message)
+
+
+@tree.command(name="999",description="何かが999倍の画像をつくる")
+async def fever(interaction: discord.Interaction, 一行目: str, 二行目: str):
+    if len_half_width(一行目) > 10 or len_half_width(二行目) > 10:
+        await interaction.response.send_message("ちょっとちょっと！\nそんな長いセリフ、めぐちゃん覚えられえないよ！\n（それぞれ全角5文字以内で入力してください）", ephemeral=True)
+        return
+    img = generate_fever_png(f"{一行目}\n{二行目}")
+    arr = io.BytesIO()
+    img.save(arr, format='PNG')
+    arr.seek(0)
+    file = discord.File(arr, filename="fever.png")
+    await interaction.response.send_message(file=file)
+
+
+def len_half_width(text):
+    return sum([(1, 2)[unicodedata.east_asian_width(char) in 'FWA'] for char in text])
 
 
 client.run(TOKEN)

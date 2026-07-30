8b67d1794265b01f1e13dd37f5eaba71579ea7ea
matsu <50922910+mtsml@users.noreply.github.com>
2026-07-31 00:28:41 +0900

    feat: update default

--- src/controllers/hellomeg/constants.ts
+++ src/controllers/hellomeg/constants.ts
@@ -61,7 +61,8 @@ export const HELLOMEG_MESSAGE_UR = `
 export const SR_MESSAGE_PREFIX = "イラスト：";
 export const HELLOMEG_JSON_PATH = "public/hellomegbot/hellomeg.json";
 
-export const HELLOMEG_HIGH_TEMPERATURE_THRESHOLD_CELSIUS_DEFAULT = 35;
+// 真夏日の30度をデフォルト
+export const HELLOMEG_HIGH_TEMPERATURE_THRESHOLD_CELSIUS_DEFAULT = 30;
 export const HELLOMEG_HIGH_TEMPERATURE_SR_RESULT = {
   filepath: "images/pine_nm/tokemeg.png",
   twitterId: "pine_nm",
